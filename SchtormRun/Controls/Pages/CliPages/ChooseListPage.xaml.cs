96ab4e50eb440529d4fa49c886e725bcaf24e511
Schtinguerch <schtinguerch@gmail.com>
2021-10-28 16:04:17 +0500

    Add: new listbox page for subWindow

---
+++ SchtormRun/Controls/Pages/CliPages/ChooseListPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace SchtormRun.Controls.Pages.CliPages
+{
+    /// <summary>
+    /// Логика взаимодействия для ChooseListPage.xaml
+    /// </summary>
+    public partial class ChooseListPage : Page
+    {
+        public delegate void SelectionChangedHandler(string selectedText);
+        public event SelectionChangedHandler SelectionChanged;
+
+        private List<string> _chooseList;
+
+        public List<string> ChooseList
+        {
+            get => _chooseList;
+            set
+            {
+                _chooseList = value;
+                FillListBox();
+            }
+        }
+
+        public ChooseListPage(List<string> chooseList)
+        {
+            InitializeComponent();
+            ChooseList = chooseList;
+
+            ItemsListBox.SelectionChanged += (o, e) => 
+                SelectionChanged?.Invoke((o as ListBox).SelectedItem.ToString());
+        }
+
+        private void FillListBox()
+        {
+            ItemsListBox.Items.Clear();
+            if (_chooseList == null || _chooseList.Count == 0)
+                return;
+
+            foreach (var item in _chooseList)
+                ItemsListBox.Items.Add(item);
+        }
+    }
+}
