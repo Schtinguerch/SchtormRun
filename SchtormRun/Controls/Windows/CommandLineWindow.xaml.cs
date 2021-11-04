0d62c1e26f0a5127fcfe3483c587453eda26feda
Schtinguerch <schtinguerch@gmail.com>
2021-11-04 11:09:53 +0500

    Add: new empty tray Add: background hook to open hidden app window (Alt+Space)

--- SchtormRun/Controls/Windows/CommandLineWindow.xaml.cs
+++ SchtormRun/Controls/Windows/CommandLineWindow.xaml.cs
@@ -4,13 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using gma.System.Windows;
 
 namespace SchtormRun.Controls.Windows
 {
@@ -19,9 +14,67 @@ namespace SchtormRun.Controls.Windows
     /// </summary>
     public partial class CommandLineWindow : Window
     {
+        private readonly BackgroundKeyboardHook _hook = new BackgroundKeyboardHook();
+        private bool _altPressed = false;
+        private bool _spacePressed = false;
+
+        public delegate void CommandLineShownHandler();
+        public event CommandLineShownHandler OnCommandLineShown;
+
         public CommandLineWindow()
         {
+            CenterNode.AppWindow = this;
+            CenterNode.SubWindow = new SubWindow();
+            CenterNode.NotificationWindow = new NotificationWindow();
+
+            CenterNode.Initialize();
             InitializeComponent();
+            
+            _hook.KeyDown += BackgroundKeyDown;
+            _hook.KeyUp += BackgroundKeyUp;
+        }
+
+        private void BackgroundKeyUp(object sender, System.Windows.Forms.KeyEventArgs e) =>
+            SetPressedState(false, e);
+
+        private void BackgroundKeyDown(object sender, System.Windows.Forms.KeyEventArgs e) =>
+            SetPressedState(true, e);
+
+        private void SetPressedState(bool value, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyValue == 164)
+            {
+                _altPressed = value;
+                return;
+            }
+
+            if (e.KeyCode == System.Windows.Forms.Keys.Space)
+            {
+                _spacePressed = value;
+            }
+
+            if (_altPressed && _spacePressed)
+            {
+                _spacePressed = false;
+                _altPressed = false;
+
+                Show();
+                OnCommandLineShown?.Invoke();
+            }
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Hide();
+                CenterNode.SubWindow.Hide();
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.Alt && e.SystemKey == Key.Space)
+            {
+                e.Handled = true;
+            }
         }
     }
 }
