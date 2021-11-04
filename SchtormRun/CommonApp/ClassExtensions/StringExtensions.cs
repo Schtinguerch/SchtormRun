a18ea8d74f3866ae281ed7d1e11508bb73c1aff1
Schtinguerch <schtinguerch@gmail.com>
2021-11-04 11:06:14 +0500

    Add: class for extensions

---
+++ SchtormRun/CommonApp/ClassExtensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace SchtormRun
+{
+    public static class StringExtensions
+    {
+        public static Uri RelativeToAppUri(this string path) =>
+            new Uri(string.Format("pack://application:,,,/{0};component/{1}",
+                Assembly.GetExecutingAssembly().GetName().Name,
+                path));
+    }
+}
