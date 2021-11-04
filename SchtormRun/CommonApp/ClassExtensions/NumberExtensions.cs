f593caf6a3fad66379a5b4d3f3ea705939ae73af
Schtinguerch <schtinguerch@gmail.com>
2021-11-04 22:12:09 +0500

    Add: new extensions for numbers (and 2 functions: Min, Max)

---
+++ SchtormRun/CommonApp/ClassExtensions/NumberExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchtormRun
+{
+    public static class NumberExtensions
+    {
+        public static double Max(this double x, double y)
+        {
+            if (x > y)
+                return x;
+
+            return y;
+        }
+
+        public static double Min(this double x, double y)
+        {
+            if (x < y)
+                return x;
+
+            return y;
+        }
+    }
+}
