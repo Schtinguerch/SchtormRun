d89028e5a10bef703b4977ccac5da4ddb287a287
Schtinguerch <schtinguerch@gmail.com>
2021-11-04 22:13:21 +0500

    Add: new interface for all autocompletion rule classes

---
+++ SchtormRun/AppFunctionality/Commands/IRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchtormRun
+{
+    public interface IRule
+    {
+        bool PreviousInputSuits(string input);
+        string LastInputCharacter { get; set; }
+        List<string> AutocompleteWords { get; set; }
+        List<string> AutocompleteDescriptions { get; set; }
+        List<string> AutocompleteIcons { get; set; }
+    }
+}
