8febc2effb6b0623923fcd60215693b6eea346a0
Schtinguerch <schtinguerch@gmail.com>
2021-11-04 22:15:17 +0500

    Add: new static class with default highlighting schemas

---
+++ SchtormRun/AppFunctionality/CodeInput/Highlighting.cs
@@ -0,0 +1,31 @@
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+
+using System;
+using System.Xml;
+using SchtormRun.Properties;
+
+namespace SchtormRun
+{
+    public static class Highlighting
+    {
+        public static IHighlightingDefinition FromFile(string path)
+        {
+            IHighlightingDefinition definition = null;
+
+            using (var reader = new XmlTextReader(path))
+            {
+                definition = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                reader.Close();
+            }
+
+            return definition;
+        }
+
+        public static IHighlightingDefinition StandardCommands =>
+            FromFile(Settings.Default.StandardCommandsXmlPath);
+
+        public static IHighlightingDefinition CalculatorCharacters =>
+            FromFile(Settings.Default.CalculatorCharactersXmlPath);
+    }
+}
