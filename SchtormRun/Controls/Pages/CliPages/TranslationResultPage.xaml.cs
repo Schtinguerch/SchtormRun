898ece83514354876f7ffbce617a92d5038b18a8
Schtinguerch <schtinguerch@gmail.com>
2021-11-04 11:08:47 +0500

    Add: new page for subWindow to show text translation results and 2 activities (to copy translated text, to enter the url in google translator)

---
+++ SchtormRun/Controls/Pages/CliPages/TranslationResultPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Diagnostics;
+
+using Localization = SchtormRun.Resources.Dictionaries.Localization.Resources;
+
+namespace SchtormRun.Controls.Pages.CliPages
+{
+    /// <summary>
+    /// Логика взаимодействия для TranslationResultPage.xaml
+    /// </summary>
+    public partial class TranslationResultPage : Page
+    {
+        private string _url;
+        private string _text;
+
+        public TranslationResultPage(string url, string text)
+        {
+            InitializeComponent();
+            TranslationResultTextBox.Text = text;
+
+            _url = url;
+            _text = text;
+        }
+
+        private void CopyTranslationButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(_text);
+                CenterNode.NotificationWindow.ShowSuccess(Localization.TextCopiedToClipboard);
+            }
+            catch (Exception ex)
+            {
+                CenterNode.NotificationWindow.ShowError($"{Localization.Error}: {ex.Message}");
+            }
+        }
+
+        private void EnterUrlButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(_url);
+        }
+    }
+}
