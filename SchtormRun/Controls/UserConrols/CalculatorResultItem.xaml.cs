0c5a036a12d7c86b6089412f090effb7b4bf6b55
Schtinguerch <schtinguerch@gmail.com>
2021-11-19 21:14:58 +0500

    Add: special calculation result item to show/copy/edit results of calculations

---
+++ SchtormRun/Controls/UserConrols/CalculatorResultItem.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SchtormRun.Controls.UserConrols
+{
+    /// <summary>
+    /// Логика взаимодействия для CalculatorResultItem.xaml
+    /// </summary>
+    public partial class CalculatorResultItem : UserControl
+    {
+        public delegate void EditReadyHandler(string expression);
+        public event EditReadyHandler OnEditReady;
+
+        private string _expression;
+
+        public CalculatorResultItem(string expresssionWithResult)
+        {
+            InitializeComponent();
+            
+            _expression = expresssionWithResult.Split('=')[0];
+            ExpressionTextEditor.Text = expresssionWithResult;
+        }
+
+        private void CopyExpressionButton_Click(object sender, RoutedEventArgs e) =>
+            Clipboard.SetText(_expression);
+
+        private void EditExpressionButton_Click(object sender, RoutedEventArgs e) =>
+            OnEditReady?.Invoke(_expression);
+    }
+}
