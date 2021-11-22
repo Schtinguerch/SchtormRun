4c338ecec0c71f30e4ddb477e87c3ff0e7584b5e
Schtinguerch <schtinguerch@gmail.com>
2021-11-22 22:18:40 +0500

    Add: extensions to write additional data for ShormRun easy for user

---
+++ SchtormRunExchange/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SchtormRunExchange.Properties;
+
+namespace SchtormRunExchange
+{
+    public static class Extensions
+    {
+        private static readonly string[] _valueTypes = new string[]
+        {
+            Settings.Default.SchtormTextType,
+            Settings.Default.SchtormMarkdownType,
+            Settings.Default.SchtormWebpageType,
+        };
+
+        public static string Include(this string baseInfo, ValueType valueType, GetValueWay getValueWay) =>
+            _valueTypes[(int)valueType] + ">>>" + (getValueWay == GetValueWay.Path ? 
+                $"{Settings.Default.SchtormPathWay}>>>" : 
+                $"{Settings.Default.SchtormCodeWay}>>>")
+            + baseInfo;
+    }
+}
