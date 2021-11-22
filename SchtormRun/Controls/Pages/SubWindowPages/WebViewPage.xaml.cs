a823f85a0f2d7075d6729adc20f072c7309f091f
Schtinguerch <schtinguerch@gmail.com>
2021-11-22 22:25:01 +0500

    Add: new subWindow page to show webpages

---
+++ SchtormRun/Controls/Pages/SubWindowPages/WebViewPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace SchtormRun.Controls.Pages.SubWindowPages
+{
+    /// <summary>
+    /// Логика взаимодействия для WebViewPage.xaml
+    /// </summary>
+    public partial class WebViewPage : Page
+    {
+        public WebViewPage(string path)
+        {
+            InitializeComponent();
+            WebViewElement.Source = new Uri(path);
+        }
+    }
+}
