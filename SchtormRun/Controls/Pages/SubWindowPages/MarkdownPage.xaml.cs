3baf494a9573c0af035501cf73de92e4d1726f77
Schtinguerch <schtinguerch@gmail.com>
2021-11-22 22:24:41 +0500

    Add: new subWindow page to show markdown documents

---
+++ SchtormRun/Controls/Pages/SubWindowPages/MarkdownPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media;
+using SchtormRun.Properties;
+
+namespace SchtormRun.Controls.Pages.SubWindowPages
+{
+    /// <summary>
+    /// Логика взаимодействия для MarkdownPage.xaml
+    /// </summary>
+    public partial class MarkdownPage : Page
+    {
+        public MarkdownPage(string path)
+        {
+            InitializeComponent();
+            MarkdownViewer.Markdown = File.ReadAllText(path);
+            MarkdownViewer.Document.Background = Brushes.White;
+
+            MarkdownViewer.Document.FontSize = Settings.Default.AppSmallFontSize;
+        }
+    }
+}
