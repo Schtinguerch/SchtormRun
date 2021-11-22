c9a150a383032f6e66bba76cc15eba88d72de97a
Schtinguerch <schtinguerch@gmail.com>
2021-11-22 22:19:22 +0500

    Add: enums for include additional information function

---
+++ SchtormRunExchange/ValueType.cs
@@ -0,0 +1,15 @@
+﻿namespace SchtormRunExchange
+{
+    public enum OutValueType
+    {
+        Text = 0,
+        Markdown = 1,
+        Webpage = 2,
+    }
+
+    public enum GetValueWay
+    {
+        Path = 0,
+        Code = 1,
+    }
+}
